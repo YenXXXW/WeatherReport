926dd01ae22401cbd317a8394cdbbe48a976a9d3
yenxxxw <yenxxxw@gmail.com>
2024-05-10 13:50:23 +0630

    contact form

--- src/app/ContactUs/page.tsx
+++ src/app/ContactUs/page.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+"use client";
 
-export default function page() {
+import React from "react";
+import { useState } from "react";
+
+export default function Page() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setName("");
+    setEmail("");
+    setMessage("");
+    setIsSubmit(true);
+  };
   return (
     <div className="container mx-auto p-4 pt-6 md:p-6 lg:p-12 md:w-[500px]">
       <h1 className="text-5xl text-center mb-10">Contact Us</h1>
       <div className="contact-form">
-        <form>
+        <form onSubmit={(e) => handleSubmit(e)}>
           <div className="form-group">
             <label htmlFor="name" className="block text-gray-700">
               Name
@@ -15,6 +30,9 @@ export default function page() {
               className="form-input w-full mt-2 rounded-md shadow-sm"
               id="name"
               placeholder="Enter your name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
             />
           </div>
           <div className="form-group">
@@ -24,8 +42,11 @@ export default function page() {
             <input
               type="email"
               className="form-input w-full mt-2 rounded-md shadow-sm"
+              required
               id="email"
               placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="form-group">
@@ -37,15 +58,29 @@ export default function page() {
               id="message"
               rows={5}
               placeholder="Enter your message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              required
             ></textarea>
           </div>
           <button
             type="submit"
-            className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-md w-full mt-5"
+            className="bg-black  text-white font-bold py-2 px-4 rounded-md w-full mt-5"
           >
             Send Message
           </button>
         </form>
+        {isSubmit && (
+          <div className="flex flex-col items-center">
+            <p>Thank you for contact us</p>
+            <button
+              className="bg-black px-2 py-1 rounded-md text-white"
+              onClick={() => setIsSubmit(false)}
+            >
+              Okay
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
